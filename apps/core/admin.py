42476b2b0c7eacd81d1fa687dd913084902db9dd
eyetheek <eyetheekhsp@gmail.com>
2026-03-01 18:03:17 +0530

    register User model to admin site

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "role", "is_active", "is_staff", "created_at")
+    search_fields = ("email",)
+    list_filter = ("role", "is_active", "created_at")
+    ordering = ("-created_at",)
