e81ab12a0594efd579ad4bfe53892ebf34dcdddb
eyetheek <eyetheekhsp@gmail.com>
2026-03-02 04:13:34 +0530

    implement pagination for dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from apps.candidates.models import Candidate
 
 
 @login_required
 def reports_dashboard(request):
-    candidates = Candidate.objects.all().order_by("-created_at")
+    candidates_qs = Candidate.objects.all().order_by("-created_at")
+
+    paginator = Paginator(candidates_qs, 10)
+    page_number = request.GET.get("page")
+    candidates_page = paginator.get_page(page_number)
 
     return render(
         request,
         "dashboard/index.html",
         {
-            "candidates": candidates,
+            "candidates": candidates_page,
         },
     )
